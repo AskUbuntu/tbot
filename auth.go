05761054ec4823260a3a9c1699eaebac13627a25
Nathan Osman <nathan@quickmediasolutions.com>
2016-09-22 14:48:49 -0700

    Add Auth type.

---
+++ auth.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"path"
+)
+
+// Auth manages access to users. This includes adding and removing users. Only
+// staff can change settings and only the administrator can perform user
+// actions.
+type Auth struct {
+	name  string
+	Users map[string]*User `json:"users"`
+}
+
+// NewAuth creates a new authenticator for registered users.
+func NewAuth(config *Config) (*Auth, error) {
+	a := &Auth{
+		name: path.Join(config.DataPath, "auth.json"),
+	}
+	e, err := LoadJSON(a.name, a)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+// Save the user list to disk.
+func (a *Auth) Save() error {
+	return SaveJSON(a.name, a)
+}
