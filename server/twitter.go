489e1a0eeabbe627fec940694e0cbb19957cfdc4
Nathan Osman <nathan@quickmediasolutions.com>
2016-10-03 15:16:04 -0700

    Add option for custom tweet.

--- server/twitter.go
+++ server/twitter.go
@@ -9,11 +9,30 @@ import (
 
 func (s *Server) twitterHandler(w http.ResponseWriter, r *http.Request) {
 	s.render(w, r, "twitter.html", pongo2.Context{
+		"title":  "Twitter",
 		"tweets": s.twitter.Tweets(),
 	})
 }
 
 func (s *Server) twitterCustomHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		message := r.Form.Get("message")
+		if message == "" {
+			s.addAlert(w, r, dangerType, "message is empty")
+		} else {
+			if err := s.twitter.Send(message); err != nil {
+				s.addAlert(w, r, dangerType, err.Error())
+			} else {
+				s.addAlert(w, r, infoType, "tweet sent successfully")
+				http.Redirect(w, r, "/twitter", http.StatusFound)
+				return
+			}
+		}
+	}
+	s.render(w, r, "twitter_custom.html", pongo2.Context{
+		"title":  "Custom Tweet",
+		"tweets": s.twitter.Tweets(),
+	})
 }
 
 func (s *Server) twitterDeleteHandler(w http.ResponseWriter, r *http.Request) {
--- www/static/js/custom.js
+++ www/static/js/custom.js
@@ -7,4 +7,15 @@ $(function() {
 
     // If the page URL matches one of the navbar links, set it as active
     $('.nav li a[href="' + location.pathname + '"]').parent().addClass('active');
+
+    // If a counter is on the page, use it
+    $('.counter').each(function() {
+        var $counter = $(this),
+            $input = $($counter.data('for'));
+        function update() {
+            $counter.text((140 - $input.val().length) + ' chars remaining');
+        }
+        $input.keyup(update);
+        update();
+    });
 });
